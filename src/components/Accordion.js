61c079715dba0ed7c20e0c91dde7ee2fd6a23b26
diego-rg <76407123+diego-rg@users.noreply.github.com>
2022-03-16 23:10:19 +0100

    accordion show item

--- src/components/Accordion.js
+++ src/components/Accordion.js
@@ -1,7 +1,8 @@
 import React, { useState } from "react";
 
 const Accordion = ({ items }) => {
-  //Hook para estado
+  //Hook para estado con array destructuring. activeIndex: valor do estado que cambiamos. setActiveIndex: función que cambia/controla ese valor do estado
+  //Cada vez que se executa o cambio volve a renderizarse, pero o valor inicial non volve a usarse. Interactúa coa clase css active, que enseña/oculta o contido
   const [activeIndex, setActiveIndex] = useState(null);
 
   //facemos unha helper function para poñer a lóxica separada e evitar ter todo o código onda o render
@@ -10,25 +11,22 @@ const Accordion = ({ items }) => {
   };
 
   const renderedItems = items.map((item, index) => {
+    const active = index === activeIndex ? "active" : "";
     //Usamos un fragmento en vez de div para evitar que o css de semantic ui nos poña 2 border
     //onClick={() => onTitleClick(index)}> fai que solo se active co click. Con onClick={onTitleClick} activaríase todo ao renderizar
     return (
       <React.Fragment key={item.title}>
-        <div className="title active" onClick={() => onTitleClick(index)}>
+        <div className={`title ${active}`} onClick={() => onTitleClick(index)}>
           <i className="dropdown icon"></i>
           {item.title}
         </div>
-        <div className="content active">
+        <div className={`content ${active}`}>
           <p>{item.content}</p>
         </div>
       </React.Fragment>
     );
   });
-  return (
-    <div className="ui styled accordion">
-      {renderedItems} <h1>{activeIndex}</h1>
-    </div>
-  );
+  return <div className="ui styled accordion">{renderedItems}</div>;
 };
 
 export default Accordion;
