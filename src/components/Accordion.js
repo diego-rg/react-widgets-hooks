70e1d8c96e8855fd9cace3e88c795779a2fd0fd7
diego-rg <76407123+diego-rg@users.noreply.github.com>
2022-03-16 20:35:13 +0100

    helper functions

--- src/components/Accordion.js
+++ src/components/Accordion.js
@@ -1,11 +1,20 @@
-import React from "react";
+import React, { useState } from "react";
 
 const Accordion = ({ items }) => {
-  const renderedItems = items.map((item) => {
+  //Hook para estado
+  const [activeIndex, setActiveIndex] = useState(null);
+
+  //facemos unha helper function para poñer a lóxica separada e evitar ter todo o código onda o render
+  const onTitleClick = (index) => {
+    setActiveIndex(index);
+  };
+
+  const renderedItems = items.map((item, index) => {
     //Usamos un fragmento en vez de div para evitar que o css de semantic ui nos poña 2 border
+    //onClick={() => onTitleClick(index)}> fai que solo se active co click. Con onClick={onTitleClick} activaríase todo ao renderizar
     return (
       <React.Fragment key={item.title}>
-        <div className="title active">
+        <div className="title active" onClick={() => onTitleClick(index)}>
           <i className="dropdown icon"></i>
           {item.title}
         </div>
@@ -15,7 +24,11 @@ const Accordion = ({ items }) => {
       </React.Fragment>
     );
   });
-  return <div className="ui styled accordion">{renderedItems}</div>;
+  return (
+    <div className="ui styled accordion">
+      {renderedItems} <h1>{activeIndex}</h1>
+    </div>
+  );
 };
 
 export default Accordion;
