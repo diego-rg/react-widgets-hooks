50c2e184373bae6c19e670a72abe5e19f81e2041
diego-rg <76407123+diego-rg@users.noreply.github.com>
2022-03-17 22:16:02 +0100

    search component

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import React from "react";
 import Accordion from "./components/Accordion";
+import Search from "./components/Search";
 
 const items = [
   {
@@ -21,6 +22,7 @@ const App = () => {
   return (
     <div>
       <Accordion items={items} />
+      <Search />
     </div>
   );
 };
