77cbe77d4127553a3369adf3781498671e2b468e
diego-rg <76407123+diego-rg@users.noreply.github.com>
2022-03-19 22:24:39 +0100

    axios request wikipedia api

--- src/components/Search.js
+++ src/components/Search.js
@@ -1,12 +1,24 @@
 import React, { useState, useEffect } from "react";
+import axios from "axios";
 
 const Search = () => {
   const [term, setTerm] = useState("");
 
   //useEffect: executa código na situación que se desexe: ao renderizar por primeira vez, por primeira e cando rerenderiza, por primeira e cando rerender e cando hai cambios nos datos
-  //o segundo argumento indica cada caso: nada, [] ou [datos]
+  //o segundo argumento indica cada caso: nada, [] ou [datos]. Non pode usar async/await na súa función, polo uque hai que usar outra dentro del
+  //(async () => {await axios.get(`${term}`)})(); é equivalente a creala en calquer variable e chamala ou usar promesas con then
   useEffect(() => {
-    console.log("term change");
+    (async () => {
+      await axios.get("https://en.wikipedia.org/w/api.php", {
+        params: {
+          action: "query",
+          list: "search",
+          origin: "*",
+          format: "json",
+          srsearch: term,
+        },
+      });
+    })();
   }, [term]);
 
   return (
