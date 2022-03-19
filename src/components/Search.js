7165c8832db629dbf072f4466c9d28f6dc6be89a
diego-rg <76407123+diego-rg@users.noreply.github.com>
2022-03-19 21:49:17 +0100

    useEffect

--- src/components/Search.js
+++ src/components/Search.js
@@ -1,7 +1,28 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 
 const Search = () => {
-  return <h1>Search</h1>;
+  const [term, setTerm] = useState("");
+
+  //useEffect: executa código na situación que se desexe: ao renderizar por primeira vez, por primeira e cando rerenderiza, por primeira e cando rerender e cando hai cambios nos datos
+  //o segundo argumento indica cada caso: nada, [] ou [datos]
+  useEffect(() => {
+    console.log("term change");
+  }, [term]);
+
+  return (
+    <div className="ui form">
+      <div className="field">
+        <label htmlFor="search-input">Enter search term</label>
+        <input
+          value={term}
+          onChange={(e) => setTerm(e.target.value)}
+          className="input"
+          type="text"
+          id="search-input"
+        />
+      </div>
+    </div>
+  );
 };
 
 export default Search;
