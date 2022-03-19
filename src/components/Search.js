3b58ce5379f0bce05e58e1a8cfcbd42c668057b6
diego-rg <76407123+diego-rg@users.noreply.github.com>
2022-03-19 23:19:49 +0100

    render api data

--- src/components/Search.js
+++ src/components/Search.js
@@ -3,13 +3,13 @@ import axios from "axios";
 
 const Search = () => {
   const [term, setTerm] = useState("");
-
+  const [results, setResults] = useState([]);
   //useEffect: executa código na situación que se desexe: ao renderizar por primeira vez, por primeira e cando rerenderiza, por primeira e cando rerender e cando hai cambios nos datos
   //o segundo argumento indica cada caso: nada, [] ou [datos]. Non pode usar async/await na súa función, polo uque hai que usar outra dentro del
   //(async () => {await axios.get(`${term}`)})(); é equivalente a creala en calquer variable e chamala ou usar promesas con then
   useEffect(() => {
-    (async () => {
-      await axios.get("https://en.wikipedia.org/w/api.php", {
+    const search = async () => {
+      const { data } = await axios.get("https://en.wikipedia.org/w/api.php", {
         params: {
           action: "query",
           list: "search",
@@ -18,8 +18,26 @@ const Search = () => {
           srsearch: term,
         },
       });
-    })();
+
+      setResults(data.query.search);
+    };
+
+    //modificamos onde chamamos á búsqueda para evitar erro debido a config da api
+    if (term) {
+      search();
+    }
   }, [term]);
+
+  const renderedResults = results.map((result) => {
+    return (
+      <div key={result.pageid} className="item">
+        <div className="content">
+          <div className="header">{result.title}</div>
+          {result.snippet}
+        </div>
+      </div>
+    );
+  });
 
   return (
     <div className="ui form">
@@ -33,6 +51,7 @@ const Search = () => {
           id="search-input"
         />
       </div>
+      <div className="ui celled list">{renderedResults}</div>
     </div>
   );
 };
