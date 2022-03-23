10c5dba056db91083aa62bbe14f1dbc48f1580d5
diego-rg <76407123+diego-rg@users.noreply.github.com>
2022-03-23 17:27:22 +0100

    React navigation without reloading

--- src/components/Link.js
+++ src/components/Link.js
@@ -2,7 +2,10 @@ import React from "react";
 
 const Link = ({ className, href, children }) => {
   const onClick = (event) => {
-    event.preventDefault();
+    event.preventDefault(); //evitar que recargue
+    window.history.pushState({}, "", href); //Cambiar a url según o compoñente
+    const navEvent = new PopStateEvent("popstate");
+    window.dispatchEvent(navEvent); //Detectan que a ur cambia
   };
 
   return (
