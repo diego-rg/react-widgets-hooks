e018764036373d3e27707411dc98b723e6c98501
diego-rg <76407123+diego-rg@users.noreply.github.com>
2022-03-23 17:31:40 +0100

    fix para ctrl click

--- src/components/Link.js
+++ src/components/Link.js
@@ -2,6 +2,10 @@ import React from "react";
 
 const Link = ({ className, href, children }) => {
   const onClick = (event) => {
+      //fix para abrir en pestaña nova con ctl+click
+      if(event.metakey || event.ctrlKey) {
+          return
+      }
     event.preventDefault(); //evitar que recargue
     window.history.pushState({}, "", href); //Cambiar a url según o compoñente
     const navEvent = new PopStateEvent("popstate");
